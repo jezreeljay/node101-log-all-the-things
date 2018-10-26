b3ba4192291994395400af45e39d42212c345583
jezbread <jezreelcallejas@jezbreads-MacBook-Pro.local>
2018-10-26 12:29:13 -0700

    Set up basic log of GET info

--- server/app.js
+++ server/app.js
@@ -2,13 +2,26 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
-app.use((req, res, next) => {
-// write your logging code here
+ignore = (req, res, next) => {
+    if (req.originalUrl === '/favicon.ico') {
+      res.status(204).json({nope: true});
+    } else {next();}
+}
 
-});
+logger = (req, res) => {    
+    // write your logging code here
+        let date = new Date().toISOString();
+        res.status(200).send('ok');
+        res.on('finish', () => {
+            console.info(`User: ${req.get('User-Agent')}, ${date}, ${req.method}, ${req.originalUrl}, ${req.httpVersion}, ${res.statusCode}`); 
+            }).end();
+        }
+
+app.use([ignore, logger]);
 
 app.get('/', (req, res) => {
 // write your code to respond "ok" here
+    logger();
 
 });
 
